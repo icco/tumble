6357f0f0086eb618d286a60629f2a995027992d7
Nat Welch <nat@natwelch.com>
2013-02-01 22:35:49 -0800

    feeds

--- models/feed.rb
+++ models/feed.rb
@@ -1,3 +1,15 @@
 class Feed < ActiveRecord::Base
 
+  def self.get_entries url
+    require 'rss'
+    require 'open-uri'
+
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      puts "Title: #{feed.channel.title}"
+      feed.items.each do |item|
+        puts "Item: #{item.title}"
+      end
+    end
+  end
 end
