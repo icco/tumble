b28d16c39e99245b04f265e4ade58d6ee85c3433
Nat Welch <nat@natwelch.com>
2013-02-05 01:06:04 -0800

    p.

--- models/feed.rb
+++ models/feed.rb
@@ -10,6 +10,7 @@ class Feed < ActiveRecord::Base
       open(self.url) do |rss|
         feed = RSS::Parser.parse(rss)
         feed.items.each do |item|
+          p item
           e = Entry.find_or_create_by_url item.link
           e.feed = self
           e.title = item.title
