ee10ce8f3c4e3dd47d5c90c2919f63d6bd7cbe4a
Nat Welch <nat@natwelch.com>
2013-02-05 22:04:54 -0800

    better time formatting.

--- app/helpers.rb
+++ app/helpers.rb
@@ -4,4 +4,8 @@ Tumble.helpers do
   def logged_in?
     return session[:loggedin] === true
   end
+
+  def t time
+    return "<time datetime=\"#{time.iso8601}\">#{time.to_s(:short)}</time>"
+  end
 end
