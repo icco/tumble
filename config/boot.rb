656f3a3eff7a0d3e017bef4d622d847aea6d0b5a
Nat Welch <nat@natwelch.com>
2013-02-05 21:07:17 -0800

    Enable honeybadger and other tweaks.

--- config/boot.rb
+++ config/boot.rb
@@ -7,28 +7,28 @@ require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, PADRINO_ENV)
 
-##
-# ## Enable devel logging
-#
-# Padrino::Logger::Config[:development][:log_level]  = :devel
-# Padrino::Logger::Config[:development][:log_static] = true
-#
-# ## Configure your I18n
-#
-# I18n.default_locale = :en
-#
-# ## Configure your HTML5 data helpers
-#
-# Padrino::Helpers::TagHelpers::DATA_ATTRIBUTES.push(:dialog)
-# text_field :foo, :dialog => true
-# Generates: <input type="text" data-dialog="true" name="foo" />
-#
-# ## Add helpers to mailer
-#
-# Mail::Message.class_eval do
-#   include Padrino::Helpers::NumberHelpers
-#   include Padrino::Helpers::TranslationHelpers
-# end
+
+Honeybadger.configure do |config|
+  config.api_key = ENV['HONEYBADGER_API_KEY']
+end
+
+## Enable devel logging
+Padrino::Logger::Config[:development][:log_level]  = :devel
+Padrino::Logger::Config[:development][:log_static] = true
+
+## Configure your I18n
+I18n.default_locale = :en
+
+## Configure your HTML5 data helpers
+Padrino::Helpers::TagHelpers::DATA_ATTRIBUTES.push(:dialog)
+text_field :foo, :dialog => true
+Generates: <input type="text" data-dialog="true" name="foo" />
+
+## Add helpers to mailer
+Mail::Message.class_eval do
+  include Padrino::Helpers::NumberHelpers
+  include Padrino::Helpers::TranslationHelpers
+end
 
 ##
 # Add your before (RE)load hooks here
