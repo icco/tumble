fe48a7e92aee256400c98d9f74b80aa66c038a10
Nat Welch <nat@natwelch.com>
2013-03-27 23:45:02 -0700

    adds word count...

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,10 @@
 $(document).ready(function() {
+  // Adds letter counter to text area.
+  $('.wordcount').each(function(i, el) {
+    var dad = $(el).parent().children('textarea')[0];
+    $(dad).bind('keypress', function() {
+      var text = $(this)[0].value;
+      $(el).text(text.length);
+    });
+  });
 });
